1283e704c7583fb43c33bf708e114d212e55c468
Morty <zaharzvagin254@gmail.com>
2026-03-28 22:15:52 +0300

    Create WeatherPlus.java

---
+++ src/main/java/com/weatherplus/WeatherPlus.java
@@ -0,0 +1,26 @@
+package com.weatherplus;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(WeatherPlus.MOD_ID)
+public class WeatherPlus {
+
+    public static final String MOD_ID = "weatherplus";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public WeatherPlus() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("WeatherPlus loaded!");
+    }
+}
